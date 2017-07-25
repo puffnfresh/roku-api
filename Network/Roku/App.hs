72ebb56c8dff5d47b18b7ff3c98afa629c655e5f
Brian McKenna <brian@brianmckenna.org>
2017-07-25 23:45:19 +1000

    Add functions for querying apps and active app

--- Network/Roku/App.hs
+++ Network/Roku/App.hs
@@ -1,6 +1,8 @@
 module Network.Roku.App where
 
+import Control.Applicative
 import Data.Text
+import Text.XML.Light.Extractors
 
 newtype AppId
   = AppId Text
@@ -8,6 +10,7 @@ newtype AppId
 
 data AppType
   = Appl
+  | Ssvr
   deriving Show
 
 newtype AppVersion
@@ -21,3 +24,39 @@ newtype AppName
 data App
   = App AppId AppType AppVersion AppName
   deriving Show
+
+data AppOrHomescreen
+  = NormalApp App
+  | Homescreen
+  deriving Show
+
+data Screensaver
+  = Screensaver App
+  deriving Show
+
+data ActiveAppResponse
+  = ActiveAppResponse AppOrHomescreen (Maybe Screensaver)
+  deriving Show
+
+appBodyParser :: ElementExtractor App
+appBodyParser = do
+  let p = Right . pack
+  i <- attribAs "id" p
+  let t' "appl" = Right Appl
+      t' "ssvr" = Right Ssvr
+      t' t = Left t
+  t <- attribAs "type" t'
+  v <- attribAs "version" p
+  n <- contents $ textAs p
+  return $ App (AppId i) t (AppVersion v) (AppName n)
+
+appsParser :: ContentsExtractor [App]
+appsParser =
+  element "apps" . children . many $ element "app" appBodyParser
+
+activeAppParser :: ContentsExtractor ActiveAppResponse
+activeAppParser =
+  element "active-app" . children $ do
+    app <- optional $ element "app" appBodyParser
+    ssvr <- optional $ element "screensaver" appBodyParser
+    return $ ActiveAppResponse (maybe Homescreen NormalApp app) $ Screensaver <$> ssvr
--- Network/Roku/Request.hs
+++ Network/Roku/Request.hs
@@ -1,12 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Network.Roku.Request where
+module Network.Roku.Request
+  ( Query(..)
+  , Method(..)
+  , Request(..)
+  , Resource(..)
+  , request
+  , resource
+  , QueryError(..)
+  , queryActiveApp
+  , queryApps
+  )
+where
 
 import Data.ByteString
 import Data.Semigroup
 import Data.Text.Encoding
+import Data.Bifunctor
 import Network.Roku.App
-import Network.Roku.Keys
+import Text.XML.Light.Extractors
+import Text.XML.Light.Input
+import qualified Data.ByteString.Lazy as LBS
 import qualified Network.HTTP.Client as HTTP
+import qualified Network.Roku.Keys as Keys
 
 data Query
   = Apps
@@ -21,9 +36,9 @@ data Method
 
 data Request
   = Query Query
-  | Keyup Key
-  | Keydown Key
-  | Keypress Key
+  | Keyup Keys.Key
+  | Keydown Keys.Key
+  | Keypress Keys.Key
   | Launch AppId
   | Install AppId
   deriving Show
@@ -40,15 +55,20 @@ resource (Query query) =
           ActiveApp -> "active-app"
           Icon (AppId appId) -> "icon/" <> encodeUtf8 appId
 resource (Keyup key) =
-  Resource Post $ "keyup/" <> keyValue key
+  Resource Post $ "keyup/" <> Keys.keyValue key
 resource (Keydown key) =
-  Resource Post $ "keydown/" <> keyValue key
+  Resource Post $ "keydown/" <> Keys.keyValue key
 resource (Keypress key) =
-  Resource Post $ "keypress/" <> keyValue key
+  Resource Post $ "keypress/" <> Keys.keyValue key
 resource (Launch (AppId appId)) =
   Resource Post $ "launch/" <> encodeUtf8 appId
 resource (Install (AppId appId)) =
   Resource Post $ "install/" <> encodeUtf8 appId
+
+data QueryError
+  = QueryInvalidXML LBS.ByteString
+  | QueryExtractionErr ExtractionErr
+  deriving Show
 
 request :: Request -> ByteString -> HTTP.Request
 request r host =
@@ -61,3 +81,19 @@ request r host =
         method' = case method of
           Post -> "POST"
           Get -> "GET"
+
+runQuery :: Query -> ContentsExtractor a -> ByteString -> HTTP.Manager -> IO (Either QueryError a)
+runQuery q p host manager = do
+  r <- HTTP.httpLbs (request (Query q) host) manager
+  let body = HTTP.responseBody r
+  return $ do
+    doc <- maybe (Left $ QueryInvalidXML body) Right $ parseXMLDoc body
+    first QueryExtractionErr $ extractDocContents p doc
+
+queryActiveApp :: ByteString -> HTTP.Manager -> IO (Either QueryError ActiveAppResponse)
+queryActiveApp = do
+  runQuery ActiveApp activeAppParser
+
+queryApps :: ByteString -> HTTP.Manager -> IO (Either QueryError [App])
+queryApps = do
+  runQuery Apps appsParser
